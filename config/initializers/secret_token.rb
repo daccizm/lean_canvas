b94a06197b82467171099fcaae0333bc3bb1666d
fukuishere <fukuishere@gmail.com>
2013-03-30 20:20:36 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeanCanvas::Application.config.secret_token = '803bdde9a3eea9157d18c6d437746330ceeb0911230f063b338fb911122057894d868db75eb1d6cf52b4c58043598aee2b303c187dda2a779d84c4ebdfb9618a'
